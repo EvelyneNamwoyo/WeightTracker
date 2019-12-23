d47d03c93cd963b6849656154ece4740e4414518
Evelyne Namwoyo <evelyne@laboremus.no>
2019-12-23 15:20:45 +0100

    Added code to cancel new-weight

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,13 @@ $(document).ready(function() {
   $('.add').click(function(){
     $('header').addClass('add-weight');
   });
-
+  
   $('form').submit(function(e){
     e.preventDefault();
+    $('header').removeClass('add-weight');
+  })
+
+  $('.cancel-new-weight').click(function(){
     $('header').removeClass('add-weight');
   })
 })
